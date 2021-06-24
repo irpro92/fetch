04b30f8b85e90eed002fd202b8b9c3302beb8425
Ira Maruhno <83663304+irpro92@users.noreply.github.com>
2021-06-25 00:43:14 +0300

    Update script.js

--- практическое задание/js/script.js
+++ практическое задание/js/script.js
@@ -1,8 +1,6 @@
 
 
 let forma = document.querySelector('form');
-let input = document.querySelector('input');
-let button = document.querySelector('.js-btn');
 let textResult = document.querySelector('.textResult');
 
 
@@ -10,19 +8,20 @@ forma.addEventListener("submit",function(e) {
 	e.preventDefault();
 	
 	let value = document.querySelector('input').value;
-	if (value > 10) {
+	if (value > 10 && value < 1) {
 		textResult.innerHTML = "Число вне диапазона";
-		return
+	else if(isNaN(value)) {
+		textResult.innerHTML = "Значение не число";	
 
 	} else {
 		let urlResponse = `https://picsum.photos/v2/list/?limit=${value}`
-		return fetch(urlResponse)
+		fetch(urlResponse)
 		.then(response => {
 			return response.json();
 		})
 		.then((data) => {
 
-			data.forEach(function(item, i, arr) {
+			data.forEach(function(item) {
 				let img = document.createElement('img');
 				img.style = 'top: 10px; left: 10px; width: 20%; padding: 10px';
 				textResult.append(img);
@@ -33,10 +32,3 @@ forma.addEventListener("submit",function(e) {
 });
 
 
-
-			// console.log(item.download_url);
-				// console.log(value);
-
-// let url = new URL('https://picsum.photos/v2/list/?limit=10');
-// url.searchParams.set('limit', [value]);
-// console.log(url.toString());
